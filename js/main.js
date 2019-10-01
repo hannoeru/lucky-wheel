14a59ac1292ba96989b0586add60ca99c72b332d
525hanlee <525hanlee@gmail.com>
2019-10-02 03:34:28 +0900

    UI rotate complete

--- js/main.js
+++ js/main.js
@@ -1,60 +1,93 @@
 let data = {
-  contents: [
-    {
-      icon: 'movie_filter',
-      text: 'Movie'
-    },
-    {
-      icon: 'cake',
-      text: 'Wish'
-    },
-    {
-      icon: 'stars',
-      text: 'Anything'
-    },
-    {
-      icon: 'child_care',
-      text: 'Child'
-    },
-    {
-      icon: 'flight',
-      text: 'Flight'
-    },
-    {
-      icon: 'wifi',
-      text: 'Wifi'
-    }
-  ],
-  year: '2017'
+  dataCache: {},
+  counts: [],
+  year: '2017',
+  isPressed: false,
+  degree: 0
 }
 
 let app = new Vue({
   el: '#wheel',
   data: data,
+  beforeMount() {
+    this.getData()
+  },
   methods: {
+    getData() {
+      var vm = this
+      axios
+        .get('./data.json')
+        .then(function(res) {
+          const keys = Object.keys(res.data)
+          keys.forEach(item => {
+            vm.$set(vm.dataCache, item, res.data[item])
+          })
+          const thisYear = vm.dataCache[2018]
+          for (let i = 0; i < thisYear.length; i++) {
+            const keys = Object.keys(thisYear[i])
+            if (keys.indexOf('start') !== -1 && keys.indexOf('end') !== -1) {
+              const start = thisYear[i].start
+              const end = thisYear[i].end
+              const count = thisYear[i].count
+              let cache = []
+              for (let j = start; j <= end; j++) {
+                cache.push({
+                  num: j,
+                  count: count
+                })
+              }
+              thisYear.splice(i + 1, 0, ...cache)
+              thisYear.splice(i, 1)
+            }
+          }
+          console.log(res.data)
+        })
+        .catch(function(err) {
+          const { status, statusText } = error.response
+          console.log(`Error! HTTP Status: ${status} ${statusText}`)
+        })
+    },
     transformHandler(index) {
-      let rotateFrom = this.year === '2017' ? -30 : 0
-      let rotate = this.year === '2017' ? 60 : 18
-      let skewY = this.year === '2017' ? -30 : -72
+      let len = this.dataCache[this.year].length
+      let rotate = 360 / len
+      let rotateFrom = -rotate / 2
+      let skewY = rotate - 90
+      if (index === 0) {
+        console.log(
+          '數量：' + len,
+          '開始角度：' + rotateFrom,
+          '旋轉角度：' + rotate,
+          '頃斜角度：' + skewY
+        )
+      }
       return `rotate(${rotateFrom + index * rotate}deg) skewY(${skewY}deg)`
     },
     generateContent() {
-      if (this.year === '2017') {
-        return this.contents
-      }
-      if (this.year === '2018') {
-        let content = []
-        for (let i = 0; i < 20; i++) {
-          content.push(i)
-        }
-        return content
-      }
+      return this.dataCache[this.year]
     },
-    checkYear() {
-      return this.year === '2017' ? true : false
+    textOrNumber(item) {
+      return this.year === '2017' ? item.text : item.num
     },
     changeYear() {
+      if (this.isPressed) return
       this.year === '2017' ? (this.year = '2018') : (this.year = '2017')
+    },
+    generateCounts() {},
+    pressHandler() {
+      if (this.isPressed) return
+      this.isPressed = true
+      let len = this.dataCache[this.year].length
+      let index = Math.floor(Math.random() * len)
+      console.log('獎項：' + (index + 1))
+      let circle = 6
+      let rotate = circle * 360 + index * (360 / len)
+      this.degree += rotate - (this.degree % 360)
+      setTimeout(() => {
+        this.isPressed = false
+      }, 6000)
+    },
+    rotateHandler() {
+      return `rotate(${this.degree}deg)`
     }
   }
 })
